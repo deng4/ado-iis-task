e067c59a2e87a376424ad131e94f48b5fa2a2e7a
deng4 <dnsoika@gmail.com>
2025-11-24 10:10:48 +0100

    all needed files for the SDK is added

--- IISDeployTask/deploy.ps1
+++ IISDeployTask/deploy.ps1
@@ -7,24 +7,24 @@ param(
 )
 
 Write-Host "Copying artifact to IIS server..."
-Copy-Item -Path $artifactPath -Destination "\\$iisServer\$targetPath" -Recurse -Force
+# Copy-Item -Path $artifactPath -Destination "\\$iisServer\$targetPath" -Recurse -Force
 
 Write-Host "Stopping App Pool..."
-Invoke-Command -ComputerName $iisServer -ScriptBlock {
-  Import-Module WebAdministration
-  Stop-WebAppPool -Name $using:appPoolName
-}
+# Invoke-Command -ComputerName $iisServer -ScriptBlock {
+#   Import-Module WebAdministration
+#   Stop-WebAppPool -Name $using:appPoolName
+# }
 
 Write-Host "Replacing folder contents..."
-Invoke-Command -ComputerName $iisServer -ScriptBlock {
-  Remove-Item "$using:targetPath\*" -Recurse -Force
-  Copy-Item "$using:artifactPath\*" "$using:targetPath" -Recurse -Force
-}
+# Invoke-Command -ComputerName $iisServer -ScriptBlock {
+#   Remove-Item "$using:targetPath\*" -Recurse -Force
+#   Copy-Item "$using:artifactPath\*" "$using:targetPath" -Recurse -Force
+# }
 
 Write-Host "Starting App Pool..."
-Invoke-Command -ComputerName $iisServer -ScriptBlock {
-  Start-WebAppPool -Name $using:appPoolName
-}
+# Invoke-Command -ComputerName $iisServer -ScriptBlock {
+#   Start-WebAppPool -Name $using:appPoolName
+# }
 
 Write-Host "Warming up IIS site..."
-Invoke-WebRequest -Uri $warmupUrl -UseBasicParsing
+#Invoke-WebRequest -Uri $warmupUrl -UseBasicParsing
